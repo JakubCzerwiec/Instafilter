a08e3364828536c13f3f068a50c1f97649bda4b3
JakubCzerwiec <jacob.czerwiec@gmail.com>
2024-12-05 20:11:21 +0100

    Challenge 2 and 3 for day 67.

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -14,6 +14,10 @@ import SwiftUI
 struct ContentView: View {
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
+    @State private var filterRadius = 0.5
+    @State private var filterScale = 0.5
+    
+    @State private var filterName = "Sepia Tone"
     
     @State private var selectedItem: PhotosPickerItem?
     @State private var showingFilters = false
@@ -21,11 +25,16 @@ struct ContentView: View {
     @AppStorage("filterCount") var filterCount = 0
     @Environment(\.requestReview) var requestReview
     
-    var enabledControls: Bool {
+    var enabledChangeFilter: Bool {
         if processedImage != nil { false } else { true }
     }
     
+    @State private var enableIntesity = true
+    @State private var enableRadius = true
+    @State private var enableScale = true
+    
     @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
+    @State private var currentFilter2: CIFilter = CIFilter.pointillize()
     let context = CIContext()
     
     var body: some View {
@@ -35,9 +44,13 @@ struct ContentView: View {
                 
                 PhotosPicker(selection: $selectedItem) {
                     if let processedImage {
-                        processedImage
-                            .resizable()
-                            .scaledToFit()
+                        VStack {
+                            Text("\(filterName)")
+                            Spacer()
+                            processedImage
+                                .resizable()
+                                .scaledToFit()
+                        }
                     } else {
                         ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
                     }
@@ -48,15 +61,24 @@ struct ContentView: View {
                 Spacer()
                 
                 HStack {
-                    Text("Intensity")
-                    Slider(value: $filterIntensity)
-                        .onChange(of: filterIntensity, applyProcessing)
-                        .disabled(enabledControls)
+                    VStack {
+                        Text("Intensity")
+                        Slider(value: $filterIntensity)
+                            .onChange(of: filterIntensity, applyProcessing)
+                            .disabled(enableIntesity)
+                        Text("Radius")
+                        Slider(value: $filterRadius)
+                            .onChange(of: filterRadius, applyProcessing)
+                            .disabled(enableRadius)
+                        Text("Scale")
+                        Slider(value: $filterScale)
+                            .disabled(enableScale)
+                    }
                 }
                 
                 HStack {
                     Button("Change filter", action: changeFilter)
-                        .disabled(enabledControls)
+                        .disabled(enabledChangeFilter)
                     
                     Spacer()
                     
@@ -68,13 +90,26 @@ struct ContentView: View {
             .padding([.horizontal, .bottom])
             .navigationTitle("Instafilter")
             .confirmationDialog("Select a filter", isPresented: $showingFilters) {
-                Button("Crystallize") { setFilter(CIFilter.crystallize() )}
-                Button("Edges") { setFilter(CIFilter.edges() )}
-                Button("Gausian Blur") { setFilter(CIFilter.gaussianBlur() )}
-                Button("Pixellate") { setFilter(CIFilter.pixellate() )}
-                Button("Sepia Tone") { setFilter(CIFilter.sepiaTone() )}
-                Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask() )}
-                Button("Vignette") { setFilter(CIFilter.vignette() )}
+                Button("Crystallize") { setFilter(CIFilter.crystallize() )
+                    filterName = "Crystallize"}
+                Button("Edges") { setFilter(CIFilter.edges() )
+                    filterName = "Edges"}
+                Button("Gausian Blur") { setFilter(CIFilter.gaussianBlur() )
+                    filterName = "Gausian Blur"}
+                Button("Pixellate") { setFilter(CIFilter.pixellate() )
+                    filterName = "Pixellate"}
+                Button("Sepia Tone") { setFilter(CIFilter.sepiaTone() )
+                    filterName = "Sepia Tone"}
+                Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask() )
+                    filterName = "Unsharp Mask"}
+                Button("Vignette") { setFilter(CIFilter.vignette() )
+                    filterName = "Vignette"}
+                Button("Depth of Field") { setFilter(CIFilter.depthOfField() )
+                    filterName = "Depth of Field"}
+                Button("Sharpen Luminance") { setFilter(CIFilter.sharpenLuminance() )
+                    filterName = "Sharpen Luminance"}
+                Button("Pointillize") { setFilter(CIFilter.pointillize() )
+                    filterName = "Pointillize"}
                 Button("Cancel", role: .cancel) { }
             }
         }
@@ -100,13 +135,16 @@ struct ContentView: View {
         
         if inputKeys.contains(kCIInputIntensityKey) {
             currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
-        }
+            enableIntesity = false
+        } else {enableIntesity = true}
         if inputKeys.contains(kCIInputRadiusKey) {
-            currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey)
-        }
+            currentFilter.setValue(filterRadius * 200, forKey: kCIInputRadiusKey)
+            enableRadius = false
+        } else {enableRadius = true}
         if inputKeys.contains(kCIInputScaleKey) {
-            currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey)
-        }
+            currentFilter.setValue(filterScale * 10, forKey: kCIInputScaleKey)
+            enableScale = false
+        } else {enableScale = true}
         
         guard let outputImage = currentFilter.outputImage else { return }
         guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
@@ -124,8 +162,6 @@ struct ContentView: View {
             requestReview()
         }
     }
-    
-
 }
 
 #Preview {
